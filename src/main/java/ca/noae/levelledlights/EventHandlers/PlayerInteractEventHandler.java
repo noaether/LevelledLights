7cdae2566c1d3b15f8987eae9e7bb414052e0ba8
noaether <67566122+noaether@users.noreply.github.com>
2024-03-06 23:48:29 -0500

    get things working, interacting with blocks is working, air isnt
    Signed-off-by: noaether <67566122+noaether@users.noreply.github.com>

--- src/main/java/ca/noae/levelledlights/EventHandlers/PlayerInteractEventHandler.java
+++ src/main/java/ca/noae/levelledlights/EventHandlers/PlayerInteractEventHandler.java
@@ -6,6 +6,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 public class PlayerInteractEventHandler implements Listener {
@@ -22,13 +23,13 @@ public class PlayerInteractEventHandler implements Listener {
         Player eventPlayer = event.getPlayer();
         Block eventBlock = event.getClickedBlock();
 
-        String chunkCoordsX = String.valueOf(eventBlock.getChunk().getX());
-        String chunkCoordsZ = String.valueOf(eventBlock.getChunk().getZ());
+        String chunkCoordsX = String.valueOf(eventBlock.getX());
+        String chunkCoordsZ = String.valueOf(eventBlock.getZ());
 
         if(event.getAction() == Action.RIGHT_CLICK_AIR) {
             plugin.getLogger().info(eventPlayer.getName() + " clicked on air at " + chunkCoordsX + "/" + chunkCoordsZ);
         } else if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
-            plugin.getLogger().info(eventPlayer.getName() + " clicked on block at " + chunkCoordsX + "/" + chunkCoordsZ);
+            plugin.getLogger().info(eventPlayer.getName() + " clicked on block at " + chunkCoordsX + "/" + chunkCoordsZ + " which is " + eventBlock.getBlockData().getPlacementMaterial().name());
         }
 
         event.setCancelled(false);

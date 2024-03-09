a1c22c4e006fd23fc591165bcccf49082ed4e7f6
noaether <67566122+noaether@users.noreply.github.com>
2024-03-09 17:33:57 -0500

    spawn falling block to make block edges glow
    Signed-off-by: noaether <67566122+noaether@users.noreply.github.com>

--- src/main/java/ca/noae/levelledlights/EventHandlers/PlayerInteractEventHandler.java
+++ src/main/java/ca/noae/levelledlights/EventHandlers/PlayerInteractEventHandler.java
@@ -1,48 +1,68 @@
 package ca.noae.levelledlights.EventHandlers;
 
 import ca.noae.levelledlights.LevelledLights;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.FallingBlock;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
-import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.util.Vector;
+import org.bukkit.event.player.PlayerMoveEvent;
 
+import java.util.Collection;
 import java.util.List;
-import java.util.Set;
 
 public class PlayerInteractEventHandler implements Listener {
 
-    private final LevelledLights plugin;
-
     public PlayerInteractEventHandler(LevelledLights plugin){
-        this.plugin = plugin;
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
     }
 
     @EventHandler
     public void onClick(PlayerInteractEvent event) {
-        //check if player is right clicking a block; if not, its not our problem
+        //check if player is right-clicking a block; if not, it's not our problem
         if(event.getAction() != Action.RIGHT_CLICK_BLOCK) {
             return;
         }
 
         Player eventPlayer = event.getPlayer();
-        Block eventBlock = event.getClickedBlock();
 
-        String chunkCoordsX = String.valueOf(eventBlock.getX());
-        String chunkCoordsZ = String.valueOf(eventBlock.getZ());
+        List<Block> blocksInSight = eventPlayer.getLineOfSight(null, 100);
 
-        Set<Material> transparentBlocks = null;
-
-        List<Block> blocksInSight = eventPlayer.getLineOfSight(null, 10);
         for(Block block : blocksInSight) {
-            eventPlayer.sendMessage("Block: " + block.getType().name());
+            if(block.getType() == Material.LIGHT) {
+                eventPlayer.sendMessage("You clicked a light!");
+                eventPlayer.getWorld().spawnParticle(org.bukkit.Particle.FLAME, block.getLocation(), 10);
+                return;
+            }
         }
 
         event.setCancelled(false);
+    }
+
+    @EventHandler
+    public void onMove(PlayerMoveEvent event) {
+        Player eventPlayer = event.getPlayer();
+        Block lookingAt = eventPlayer.getTargetBlock(null, 10);
+        if(lookingAt.getType() == Material.LIGHT) {
+            Location blockLoc = new Location(eventPlayer.getWorld(), lookingAt.getX() + 0.5, lookingAt.getY() + 0.5, lookingAt.getZ() + 0.5);
+            Collection<FallingBlock> pre_shulkerCollection = eventPlayer.getWorld().getNearbyEntitiesByType(FallingBlock.class, blockLoc.add(0, 0, 0), 5);
+
+            if(pre_shulkerCollection.isEmpty()){
+                eventPlayer.getWorld().spawnEntity(blockLoc.add(0, 1, 0), EntityType.FALLING_BLOCK);
+
+                eventPlayer.getWorld().getEntitiesByClass(FallingBlock.class).forEach(glowBlock -> {
+                    glowBlock.setGravity(false);
+                    glowBlock.teleport(glowBlock.getLocation().add(0, 0, 0));
+                    glowBlock.setGlowing(true);
+                });
+            }
+
+            eventPlayer.getWorld().spawnParticle(org.bukkit.Particle.FLAME, blockLoc, 100);
+        }
     }
 }

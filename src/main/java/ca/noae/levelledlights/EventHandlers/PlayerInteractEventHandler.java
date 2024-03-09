cbc6793eb985c043d9cfdaef3028f8caaf8d5cd9
noaether <67566122+noaether@users.noreply.github.com>
2024-03-09 18:11:03 -0500

    avoid redundant checks + use UUID to avoid mishaps
    Signed-off-by: noaether <67566122+noaether@users.noreply.github.com>

--- src/main/java/ca/noae/levelledlights/EventHandlers/PlayerInteractEventHandler.java
+++ src/main/java/ca/noae/levelledlights/EventHandlers/PlayerInteractEventHandler.java
@@ -14,21 +14,21 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.scheduler.BukkitRunnable;
 
-import java.util.Collection;
 import java.util.List;
+import java.util.UUID;
 
 public class PlayerInteractEventHandler implements Listener {
 
     private static LevelledLights plugin;
 
-    public PlayerInteractEventHandler(LevelledLights plugin){
+    public PlayerInteractEventHandler(LevelledLights plugin) {
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
     }
 
     @EventHandler
     public void onClick(PlayerInteractEvent event) {
         //check if player is right-clicking a block; if not, it's not our problem
-        if(event.getAction() != Action.RIGHT_CLICK_BLOCK) {
+        if (event.getAction() != Action.RIGHT_CLICK_BLOCK) {
             return;
         }
 
@@ -36,8 +36,8 @@ public class PlayerInteractEventHandler implements Listener {
 
         List<Block> blocksInSight = eventPlayer.getLineOfSight(null, 100);
 
-        for(Block block : blocksInSight) {
-            if(block.getType() == Material.LIGHT) {
+        for (Block block : blocksInSight) {
+            if (block.getType() == Material.LIGHT) {
                 eventPlayer.sendMessage("You clicked a light!");
                 eventPlayer.getWorld().spawnParticle(org.bukkit.Particle.FLAME, block.getLocation(), 10);
                 return;
@@ -51,31 +51,27 @@ public class PlayerInteractEventHandler implements Listener {
     public void onMove(PlayerMoveEvent event) {
         Player eventPlayer = event.getPlayer();
         Block lookingAt = eventPlayer.getTargetBlock(null, 10);
-        if(lookingAt.getType() == Material.LIGHT) {
+        if (lookingAt.getType() == Material.LIGHT) {
             Location blockLoc = new Location(eventPlayer.getWorld(), lookingAt.getX() + 0.5, lookingAt.getY() + 0.5, lookingAt.getZ() + 0.5);
-            Collection<Slime> pre_shulkerCollection = eventPlayer.getWorld().getNearbyEntitiesByType(Slime.class, blockLoc.add(0, 0, 0), 5);
 
-            if(pre_shulkerCollection.isEmpty()){
-                eventPlayer.getWorld().spawnEntity(blockLoc, EntityType.SLIME);
+            UUID glowBlockID = eventPlayer.getWorld().spawnEntity(blockLoc.add(0, -0.5, 0), EntityType.SLIME).getUniqueId();
+            Slime glowBlock = (Slime) eventPlayer.getWorld().getEntity(glowBlockID);
 
-                eventPlayer.getWorld().getEntitiesByClass(Slime.class).forEach(glowBlock -> {
-                    glowBlock.setSize(1);
-                    glowBlock.setGravity(false);
-                    glowBlock.setAI(false);
-                    glowBlock.setInvulnerable(true);
-                    glowBlock.setInvisible(true);
-                    glowBlock.setCollidable(false);
-                    glowBlock.setSilent(true);
-                    glowBlock.setGlowing(true);
+            glowBlock.setSize(2);
+            glowBlock.setGravity(false);
+            glowBlock.setAI(false);
+            glowBlock.setInvulnerable(true);
+            glowBlock.setInvisible(true);
+            glowBlock.setCollidable(false);
+            glowBlock.setSilent(true);
+            glowBlock.setGlowing(true);
 
-                    new BukkitRunnable() {
-                        @Override
-                        public void run() {
-                            glowBlock.remove();
-                        }
-                    }.runTaskLater(LevelledLights.getInstance(), 20 * 5);
-                });
-            }
+            new BukkitRunnable() {
+                @Override
+                public void run() {
+                    glowBlock.remove();
+                }
+            }.runTaskLater(LevelledLights.getInstance(), 20 * 5);
         }
     }
 }
